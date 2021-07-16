5af6fb2fa8794e36651261067e8704a7899f1302
Jenna Ransom <jenna.ransom@gmail.com>
2021-07-16 16:08:51 -0600

    adds a type size range, styles range input and wires up js for size changes

--- typechecker.js
+++ typechecker.js
@@ -4,6 +4,10 @@ document.addEventListener('DOMContentLoaded', () => {
   const outputTag = document.querySelector('textarea.output')
   const ogText = outputTag.value
 
+  const typesizeTag = document.querySelector(`input[name="typesize"]`)
+  const typesizeOutput = document.querySelector('span.typesize-output') 
+
+  //change the display sentence 
   sentenceTag.addEventListener('keyup', function() {
     (this.value ? outputTag.value = this.value : outputTag.value = ogText) 
   })
@@ -12,5 +16,10 @@ document.addEventListener('DOMContentLoaded', () => {
     sentenceTag.value = this.value
   })
 
+  // change the type size 
+  typesizeTag.addEventListener('input', (e) => {
+    outputTag.style.fontSize = e.target.value + 'px'
+    typesizeOutput.innerHTML = e.target.value + 'px'
+  })
 
 })
